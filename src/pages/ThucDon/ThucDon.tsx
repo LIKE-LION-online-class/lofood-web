7f540679fbedaecddaf10a491d5b9363c3a1452c
ngocvon <ngocvonkh@gmail.com>
2024-06-28 17:45:40 +0700

    add-food-menu

--- src/pages/ThucDon/ThucDon.tsx
+++ src/pages/ThucDon/ThucDon.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react';
 import { Box, Container, Grid, Typography } from '@mui/material';
-import { Link ,browserHistory} from 'react-router-dom';
 import NewProductCard from '@/components/NewProductCard.tsx';
 import Slider from 'react-slick';
+import Link from '@mui/material/Link';
 import DanhMucMonAn from '@/components/DanhMucMonAn.tsx';
 
 var settingsCarousel = {
@@ -12,82 +12,98 @@ var settingsCarousel = {
   slidesToShow: 6,
   slidesToScroll: 1
 };
-const setLocation=(e)=>{
-  e.preventDefault();
-  window.history.pushState({}, '', '/dummy-page')
+let currentLink;
+const scrollNavigation = (e)=>{
+  currentLink = e.currentTarget.getAttribute('href');
+  // @ts-ignore
+  var getOffsetTop= document.getElementById(currentLink.slice(1)).offsetTop;
+  document.querySelectorAll(".boxNarrowMenu a").forEach((e)=>{
+    e.classList.remove('active');
+  })
+  e.target.classList.add('active');
+  // @ts-ignore
+  document.querySelector('body').addEventListener("scroll", (event) => {
+    console.log('event',event);
+    `scrollTop: ${getOffsetTop}`;
+  });
 }
+document.addEventListener("load",(e)=>{
+  document.querySelectorAll(".boxNarrowMenu a").forEach((e)=>{
+    e.classList.remove('active');
+  });
 
+})
 const ThucDon =() => {
   return (
         <Container maxWidth="lg" sx={{paddingTop:'100px'}}>
-          <Box className="fix-button-slick" sx={{ overflow: 'hidden'}} my={6}>
+          <Box className="fix-button-slick boxNarrowMenu" sx={{ overflow: 'hidden'}} my={6}>
             <Slider {...settingsCarousel}>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }} onClick={setLocation} rel='noopener noreferrer' >
+                  <Link id="boxNarrowMenuFirst" href="#uu-dai" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}  onClick={scrollNavigation}>
                     Ưu đãi
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }} >
+                  <Link  href="#mon-moi" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }} onClick={scrollNavigation}>
                     Món mới
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black'}}>
+                  <Link  href="#combo-1-nguoi" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px'}} onClick={scrollNavigation}>
                     Combo 1 người
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }}>
+                  <Link  href="#combo-nhom" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}>
                     Combo nhóm
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }}>
+                  <Link  href="#ga-ran-ga-quay" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}>
                     Gà Rán - Gà Quay
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }}>
+                  <Link  href="#thuc-an-nhe" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}>
                     Thức Ăn Nhẹ
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }}>
+                  <Link  href="#burger-com-mi-y" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}>
                     Burger - Cơm - Mì Ý
                   </Link>
                 </Typography>
               </Box>
               <Box p={1}>
                 <Typography variant="h6" noWrap component="div" textAlign="center">
-                  <Link style={{ textDecoration: 'none', color: 'black' }}>
+                  <Link  href="#thuc-uong-trang-mieng" underline="none" style={{ textDecoration: 'none', color: 'black',paddingBottom:'5px' }}>
                     Thức Uống & Trang Miệng
                   </Link>
                 </Typography>
               </Box>
             </Slider>
           </Box>
-          <Box className="uu-dai" my={6} href="/uu-dai">
+          <Box className="uu-dai" my={6} id="uu-dai">
             <Typography variant="h2" noWrap component="h2" my={4} sx={{position:'relative'}}>
               <span>Ưu đãi</span>
             </Typography>
             <Box
               sx={{
                 display: 'grid',
-                gap: 4,
+                gap: 2,
                 gridTemplateColumns: 'repeat(4, 1fr)',
               }}
             >
@@ -113,14 +129,14 @@ const ThucDon =() => {
               </Grid>
             </Box>
           </Box>
-          <Box className="mon-moi" my={6} href="/mon-moi">
+          <Box className="mon-moi" my={6} id="mon-moi">
             <Typography variant="h2" noWrap component="h2" my={4} sx={{position:'relative'}}>
               <span>Món Mới</span>
             </Typography>
             <Box
               sx={{
                 display: 'grid',
-                gap: 4,
+                gap: 2,
                 gridTemplateColumns: 'repeat(4, 1fr)',
               }}
             >
@@ -146,14 +162,14 @@ const ThucDon =() => {
               </Grid>
             </Box>
           </Box>
-          <Box className="combo-one-people" my={6} href="/combo-one-nguoi">
+          <Box className="combo-one-people" my={6} id="combo-1-nguoi">
             <Typography variant="h2" noWrap component="h2" my={4} sx={{position:'relative'}}>
               <span>Combo một người</span>
             </Typography>
             <Box
               sx={{
                 display: 'grid',
-                gap: 4,
+                gap: 2,
                 gridTemplateColumns: 'repeat(4, 1fr)',
               }}
             >
