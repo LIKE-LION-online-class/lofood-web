6a13d8c09d7d9163e80e15ac2e15e12953c7474e
Duy Phong <nguyenduyphong1504@gmail.com>
2024-07-03 19:03:38 +0700

    code ApiClientProvider, check error 401

--- src/router/Routing.tsx
+++ src/router/Routing.tsx
@@ -4,50 +4,52 @@ import Login from '@/pages/Auth/Login';
 import HeThongNhaHang from '@/pages/HeThongNhaHang/HeThongNhaHang';
 import Home from '@/pages/Home/Home';
 import MainLayout from '@/components/MainLayout';
-import {Outlet, createBrowserRouter,RouteObject} from "react-router-dom";
+import { Outlet, createBrowserRouter, RouteObject } from 'react-router-dom';
 import ThucDon from '@/pages/ThucDon/ThucDon.tsx';
 import GioHang from '@/pages/GioHang/GioHang.tsx';
+import { ApiClientProvider } from '@/utils/axios';
 
 const appRoutes: RouteObject[] = [
   {
     path: '/',
-    element: <MainLayout/>,
-    children:[
+    element: <MainLayout />,
+    children: [
       {
         index: true,
-        element: <Home />
+        element: <Home />,
       },
       {
         path: '/thuc-don',
-        element:<ThucDon />
+        element: <ThucDon />,
       },
       {
         path: '/he-thong-nha-hang',
-        element: <HeThongNhaHang />
+        element: <HeThongNhaHang />,
       },
       {
         path: '/gio-hang',
-        element: <GioHang />
+        element: <GioHang />,
       },
       {
         path: '/auth/login',
-        element: <Login />
+        element: <Login />,
       },
       {
         path: '/auth/register',
-        element: <Register />
-      }
-    ]
-  }
+        element: <Register />,
+      },
+    ],
+  },
 ];
 const Routing = createBrowserRouter([
   {
-    element:(
-      <Outlet/>
+    element: (
+      <ApiClientProvider>
+        <Outlet />
+      </ApiClientProvider>
     ),
-    children:appRoutes
-  }
-])
-
+    children: appRoutes,
+  },
+]);
 
 export default Routing;
