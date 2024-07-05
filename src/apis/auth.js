02d13083b7588c882ef9ea2c21084e630d4ace09
ngocvon <ngocvonkh@gmail.com>
2024-07-05 09:01:47 +0700

    had form forgot password

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -7,3 +7,7 @@ export const loginHttp = (data) => {
 export const registerHttp = (data) => {
   return instance.post('/security/register', data)
 }
+
+export const forgotPasswordHttp = (data) => {
+  return instance.post('/security/forgetPassword', data)
+}
