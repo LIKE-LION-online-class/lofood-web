f718e1fb2d8f480715cdcfa6a6d27ab1444a0d2a
Le Sang <3lesang@gmail.com>
2024-07-22 23:40:55 +0700

    fix food detail

--- src/pages/HomePage/components/ListSlide.tsx
+++ src/pages/HomePage/components/ListSlide.tsx
@@ -1,6 +1,9 @@
+import { getFoodHttp } from '@/api/food';
 import { Box, Card, CardContent, Grid, IconButton, Stack, styled } from '@mui/material';
 import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react';
+import { useQuery } from '@tanstack/react-query';
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import Slick, { Settings } from 'react-slick';
 
 const StyledIconButton = styled(IconButton)(() => ({
@@ -15,10 +18,16 @@ function ListSlide() {
     infinite: false,
     autoplay: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 5,
+    slidesToScroll: 5,
   };
 
+  const { data } = useQuery({
+    queryKey: ['getFoods'],
+    queryFn: getFoodHttp,
+  });
+
+  console.log(data);
   let sliderRef = useRef<Slick>(null);
 
   const handleNext = () => {
@@ -39,46 +48,20 @@ function ListSlide() {
       >
         <CardContent>
           <Slick {...setttings} ref={sliderRef}>
-            <Box p={1}>
-              <Box
-                height={200}
-                bgcolor="primary.main"
-                borderRadius={2}
-                sx={{
-                  backgroundImage:
-                    'url(https://salt.tikicdn.com/cache/w750/ts/tikimsp/f6/ee/67/3f24bac5673e718ab1d06e3347bb65ce.png.webp)',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }}
-              />
-            </Box>
-
-            <Box p={1}>
-              <Box
-                height={200}
-                borderRadius={2}
-                bgcolor="primary.main"
-                sx={{
-                  backgroundImage:
-                    'url(https://salt.tikicdn.com/cache/w750/ts/tikimsp/43/09/ba/457624db3a9834d8f3ac7913527d2866.png.webp)',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }}
-              />
-            </Box>
-            <Box p={1}>
-              <Box
-                height={200}
-                borderRadius={2}
-                bgcolor="primary.main"
-                sx={{
-                  backgroundImage:
-                    'url(https://salt.tikicdn.com/cache/w750/ts/tikimsp/c8/5d/0b/4d947c862d9d5c567e3a2dc59f9d4213.jpg.webp)',
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }}
-              />
-            </Box>
+            {data?.data?.map((item: any) => (
+              <Box p={1} key={item?.id} component={Link} to={`/food/${item?.id}`}>
+                <Box
+                  height={200}
+                  bgcolor="primary.main"
+                  borderRadius={2}
+                  sx={{
+                    backgroundImage: `url(${item?.image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                />
+              </Box>
+            ))}
           </Slick>
 
           <Stack direction="row" justifyContent="center" spacing={1}>
